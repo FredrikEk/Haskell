f92f7eaa5be2941f456d53c051de77383f4eef84
Fredrik Ek <fredrik.ek.s@gmail.com>
2014-09-20 13:27:21 +0200

    Updated Script for more functionality

--- createMass.hs
+++ createMass.hs
@@ -1,22 +1,19 @@
 import System.Directory
+import Data.Char ( isLetter )
 
 --"Readable" Code
 
 createMass :: FilePath -> String -> IO ()
 createMass file text = 
   do s <- readFile file
-     writeFile "MassTell.txt" (addText (removeCommas s) text) 
-	 
-removeCommas :: String -> [String]
-removeCommas s = map (reverse . tail . reverse) (words s)
+     writeFile "MassTell.txt" (addText (removeNonLetters s) text) 
+
+removeNonLetters :: String -> [String]
+removeNonLetters s = map (\x -> filter(isLetter) x) (words s)
 
 addText :: [String] -> String -> String
 addText s text = 
-        (textAdder (map (\x -> "/tell " ++ x ++ " " ++ text ++ "\n" ++ "/delay 5000\n") s))
-
-textAdder :: [String] -> String
-textAdder [] = ""
-textAdder (x:xs) = x ++ (textAdder xs)
+        concat (map (\x -> "/tell " ++ x ++ " " ++ text ++ "\n" ++ "/delay 5000\n") s)
 
 --Oneliner
 
@@ -24,7 +21,6 @@ createMass2 :: FilePath -> String -> IO ()
 createMass2 file text = 
   do s <- readFile file
      writeFile "MassTell.txt" 
-	 (textAdder2 (map (\x -> "/tell " ++ x ++ " " ++ text ++ "\n" ++ "/delay 5000\n") 
-	             (map (reverse . tail . reverse) (words s)))) where
-       textAdder2 [] = ""
-       textAdder2 (x:xs) = x ++ (textAdder xs) 
+	 (concat (map (\x -> "/tell " ++ x ++ " " ++ text ++ "\n" ++ "/delay 5000\n") 
+	             (map (\x -> filter(isLetter) x) (words s))))
+       
